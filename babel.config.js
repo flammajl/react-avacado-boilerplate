9828ab9090c5c37dffad613688ac4fe3f20a5240
João Luiz <jleaopitta@gmail.com>
2021-07-11 17:17:31 -0300

    fix: jest types

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['next/babel', '@babel/preset-typescript'],
-  plugins: [['styled-components', { ssr: true }]],
+  plugins: [['styled-components', { ssr: true, displayName: true }]],
+  env: {
+    test: {
+      plugins: [['styled-components', { ssr: false, displayName: false }]],
+    },
+  },
 };
